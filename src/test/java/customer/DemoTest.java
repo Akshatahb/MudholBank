78b7ca061c576fe7576b6c70472b8e64a1aa3ebf
Akshatahb <akshatahbelagali1600@gmail.com>
2024-01-25 15:55:26 +0530

    first time changes

---
+++ src/test/java/customer/DemoTest.java
@@ -0,0 +1,21 @@
+package customer;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	public void method1Test()
+	{
+		System.out.println("------Test 1------");
+		System.out.println("Akshata");
+	}
+	
+	@Test
+	public void method2Test()
+	{
+		System.out.println("------Test 2------");
+		System.out.println("Akshata");
+	}
+
+
+}
